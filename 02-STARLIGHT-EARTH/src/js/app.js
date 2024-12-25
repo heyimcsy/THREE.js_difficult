2ace0d49ec14039caa46d42fab7d6f9bf356a97c
sychoi <cinnamon1995@naver.com>
2024-12-25 21:27:02 +0900

    지구에 3d 효과 부여

---
+++ 02-STARLIGHT-EARTH/src/js/app.js
@@ -0,0 +1,85 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+export default function () {
+  const renderer = new THREE.WebGLRenderer({
+    alpha: true,
+  });
+
+  renderer.outputEncoding = THREE.sRGBEncoding;
+  const textureLoader = new THREE.TextureLoader();
+
+  const container = document.querySelector('#container');
+
+  container.appendChild(renderer.domElement);
+
+  const canvasSize = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+  };
+
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    canvasSize.width / canvasSize.height,
+    0.1,
+    100
+  );
+  camera.position.set(0, 0, 3);
+
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.enableDamping = true;
+  controls.dampingFactor = 0.1;
+
+  const addLight = () => {
+    const light = new THREE.DirectionalLight(0xffffff);
+    light.position.set(2.65, 2.13, 1.02)
+    scene.add(light)
+  }
+
+  const createEarth1 = () => {
+    // MeshBasicMaterial     or MeshStandardMaterial 조명이 필요하다
+    const material = new THREE.MeshStandardMaterial({
+      map: textureLoader.load('assets/earth-night-map.jpg'),
+    })
+
+    //sphere geometry 는 동그란 물체를 형성할 때 제일 많이 사용한다.
+    const geometry = new THREE.SphereGeometry(1.3, 30, 30)
+
+    const mesh = new THREE.Mesh(geometry, material)
+    scene.add(mesh)
+  };
+
+  const resize = () => {
+    canvasSize.width = window.innerWidth;
+    canvasSize.height = window.innerHeight;
+
+    camera.aspect = canvasSize.width / canvasSize.height;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(canvasSize.width, canvasSize.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  };
+
+  const addEvent = () => {
+    window.addEventListener('resize', resize);
+  };
+
+  const draw = () => {
+    controls.update();
+    renderer.render(scene, camera);
+    requestAnimationFrame(() => {
+      draw();
+    });
+  };
+
+  const initialize = () => {
+    addLight();
+    createEarth1();
+    addEvent();
+    resize();
+    draw();
+  };
+
+  initialize();
+}
