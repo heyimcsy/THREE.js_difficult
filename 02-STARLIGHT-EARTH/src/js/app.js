15f10fde5e97e8165efcac160a0de2cdd7896fbd
sychoi <cinnamon1995@naver.com>
2024-12-27 16:33:07 +0900

    오브젝트의 회전 처리

--- 02-STARLIGHT-EARTH/src/js/app.js
+++ 02-STARLIGHT-EARTH/src/js/app.js
@@ -98,10 +98,12 @@ export default function () {
     );
 
     const particleMaterial = new THREE.PointsMaterial({
-      size: 0.1,
+      size: 0.01,
       transparent: true,
       map: textureLoader.load('assets/particle.png'),
-      alphaMap: textureLoader.load('assets/particle.png')
+      alphaMap: textureLoader.load('assets/particle.png'),
+      depthWrite: false,
+      color: 0xbcc6c6
     })
 
     const star = new THREE.Points(particleGeometry, particleMaterial);
@@ -113,6 +115,12 @@ export default function () {
     const earth2 = createEarth2();
     const star = createStar(1000);
     scene.add(earth1, earth2, star);
+
+    return{
+      earth1,
+      earth2,
+      star
+    }
   }
 
   const resize = () => {
@@ -130,20 +138,30 @@ export default function () {
     window.addEventListener('resize', resize);
   };
 
-  const draw = () => {
+  const draw = (obj) => {
+    const {earth1, earth2, star} = obj;
+    earth1.rotation.x += 0.0005;
+    earth1.rotation.y += 0.0005;
+
+    earth2.rotation.x += 0.0005;
+    earth2.rotation.y += 0.0005;
+
+    star.rotation.x += 0.001;
+    star.rotation.y += 0.001;
+
     controls.update();
     renderer.render(scene, camera);
     requestAnimationFrame(() => {
-      draw();
+      draw(obj);
     });
   };
 
   const initialize = () => {
     addLight();
-    create();
+    const obj = create();
     addEvent();
     resize();
-    draw();
+    draw(obj);
   };
 
   initialize();
